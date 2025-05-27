025d2497f874599ed80c4371ceffa31ee410b606
AlexBDominguez <alexwirtzfp@gmail.com>
2025-05-27 14:32:48 +0200

    Ejercicio 24 Testeado

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,25 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> queue1 = new LinkedList<>();
+        queue1.offer("Smith");
+        queue1.offer("Montessori");
+        queue1.offer("Peralta");
+        queue1.offer("House");
 
+        return queue1;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+        while(!queue.isEmpty()){
+            String elemento = queue.poll();
+            System.out.println(elemento);
+        }
 
     }
 
     public static void main(String[] args) {
-
+        Queue<String> queue = createQueue();
+        printAndEmptyQueue(queue);
     }
 
 }
